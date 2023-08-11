0146bc229ebc45ec3b659bf11bbdcf9ca20c1c12
Agu <alemosmusi@gmail.com>
2023-08-10 22:45:24 -0300

    listo 11

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,47 +1,67 @@
-import React, { useEffect } from 'react'
-import { ActivityIndicator, Dimensions, ListRenderItem, Text, View } from 'react-native'
-import { useMovies } from '../hooks/useMovies'
-import MoviePoster from '../components/MoviePoster'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import React, {useEffect} from 'react';
+import {
+  ActivityIndicator,
+  Dimensions,
+  FlatList,
+  ListRenderItem,
+  LogBox,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
+import {useMovies} from '../hooks/useMovies';
+import MoviePoster from '../components/MoviePoster';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 import Carousel from 'react-native-snap-carousel';
 
-const windowWidth = Dimensions.get('window').width
-    
+LogBox.ignoreLogs(['ViewPropTypes will be removed']);
+
+const windowWidth = Dimensions.get('window').width;
+
 const HomeScreen = () => {
+  const {peliculasEnCine, isLoading} = useMovies();
+  const {top} = useSafeAreaInsets();
 
+  if (isLoading) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignContent: 'center'}}>
+        <ActivityIndicator color="red" size={100} />
+      </View>
+    );
+  }
 
-    const { peliculasEnCine, isLoading } = useMovies()
-    const {top}=useSafeAreaInsets()
-
-
-
-    if (isLoading) {
-        return (
-            <View style={{flex:1, justifyContent: 'center', alignContent:'center'}}>
-                <ActivityIndicator color='red' size={100}/>
-            </View>
-        )
-    }
-
-   return (
-       <View style={{marginTop: top +20}}>
-        
-            <View style={{
-                height:440,
+  return (
+    <ScrollView>
+        <View style={{marginTop: top + 20}}>
+        <View
+            style={{
+            height: 440,
             }}>
-                <Carousel 
-                        data={peliculasEnCine}
-                        renderItem={({item}: any)=> <MoviePoster 
-                            movie={item}
-                        /> }
-                        sliderWidth={windowWidth}
-                        itemWidth={300}
-                
-                />
-            </View>
-       </View>
-)
-}
-    
-export default HomeScreen
+            <Carousel
+            data={peliculasEnCine}
+            renderItem={({item}: any) => <MoviePoster movie={item} />}
+            sliderWidth={windowWidth}
+            itemWidth={300}
+            />
+        </View>
+
+        <View style={{backgroundColor: 'red', height: 260}}>
+            <Text style={{fontSize: 30, fontWeight: 'bold'}}>En cine</Text>
+            <FlatList 
+                data= {peliculasEnCine}
+                renderItem={({item}:any)=> 
+                    <MoviePoster movie={item} width={140} height={200}/>
+                }
+                keyExtractor={(item)=> item.id.toString()}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+            />
+        </View>
+        </View>
+
+    </ScrollView>
+  );
+};
+
+export default HomeScreen;
---
+++ typings.d.ts
@@ -0,0 +1,9 @@
+// declare module 'react-native-snap-carousel' {
+//     import { Component } from 'react';
+    
+//     // Agrega aquí la declaración de tipo para el componente Carousel
+//     class Carousel extends Component<any> {}
+  
+//     // Exporta el componente declarado
+//     export default Carousel;
+//   }
